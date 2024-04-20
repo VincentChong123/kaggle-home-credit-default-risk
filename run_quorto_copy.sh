d7ef560b110b7a5bf8c10f46440ce2a8ce2123b0
Vincent Chong <ws.chong.sg@gmail.com>
2024-04-20 20:39:23 +0800

    at-nlb2

--- run_quorto_copy.sh
+++ run_quorto_copy.sh
@@ -1,11 +1,11 @@
-export PRJ_NAME="EDA_IMF_Airbnb"
+export PRJ_NAME="home_credit_loan_default_2018"
 export MAIN_DIR="/Users/daddy2/01-prj/tfip_da/"
 export DST_DIR=$MAIN_DIR"/vincentChong123.github.io/docs/"$PRJ_NAME
 echo $DST_DIR
 mkdir -p $DST_DIR
 
-rm -rf ./$EDA_IMF_Airbnb
-quarto render "./EDA_IMF_Airbnb.ipynb"
+
+quarto render "2018/predict.ipynb"
 # cp -rvf docs/.  ../../../../vincentchong123.github.io/docs  
 cp -rvf docs_subdir/. $DST_DIR
 rm -r docs_subdir
